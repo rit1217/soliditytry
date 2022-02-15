d237ef0092a627731e49add92e32cad6a7c7b4ab
Phurit Warapat <phuritwarapat@gmail.com>
2022-02-15 12:58:09 +0700

    get abi and bytecode

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,11 +1,11 @@
 from solcx import compile_standard
-
+import json
 
 with open("./web3_py_simple_storage/SimpleStorage.sol","r") as file:
     simple_storage_file = file.read()
     
 compiled_sol = compile_standard(
-    {
+    { 
         "language":"Solidity",
         "sources":{"SimpleStorage.sol":{"content": simple_storage_file}},
         "settings":{
@@ -16,4 +16,11 @@ compiled_sol = compile_standard(
     },
     solc_version="0.6.0",
 )
-print(compiled_sol)
+with open("compiled_cod.json","w") as file:
+    json.dump(compiled_sol, file)
+
+#get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+
+#get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
